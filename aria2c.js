c1725a090de4ef967ed0155b41f063dc65f522d3
M. Nabil Adani <nblid48@gmail.com>
2021-06-14 18:01:26 +0700

    feat: add autodownload with aria2c

---
+++ aria2c.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name                Monkey D Aria2
+// @version             1.0.0
+// @description         Automatically download file from file hosting with aria2-RCP.
+// @author              mnabila
+// @namespace           https://github.com/mnabila
+// @license             MIT License
+// @match               https://*.zippyshare.com/*
+// @match               https://anonfiles.com/*
+// @match               http://www.solidfiles.com/*
+// @grant               none
+// ==/UserScript==
+
+(function () {
+    function sendNotification(body) {
+        if (Notification.permission != 'granted') {
+            Notification.requestPermission();
+        }
+        let notif = new Notification('Aria2 | Download', { body: body });
+    }
+
+    function download(url) {
+        // TODO: fix cors, i think use mode no-cors is not fix this problem
+        const uri = 'http://localhost:6800/jsonrpc';
+        fetch(uri, {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                jsonrpc: '2.0',
+                id: Math.floor(Math.random() * 5),
+                method: 'aria2.addUri',
+                params: [[url]],
+            }),
+        });
+        sendNotification(document.title);
+    }
+
+    function zippyshare() {
+        const url = document.querySelector('a#dlbutton');
+        download(url.href);
+    }
+
+    function solidfile() {
+        const script = document
+            .evaluate(
+                "//script[contains(., 'downloadUrl')]",
+                document,
+                null,
+                XPathResult.ANY_TYPE,
+                null,
+            )
+            .iterateNext();
+        const data = script.innerHTML;
+        const result = JSON.parse(data.match('({.*})')[0]);
+        download(result.downloadUrl);
+    }
+
+    function anonfiles() {
+        const url = document.querySelector('a#download-url');
+        download(url.href);
+    }
+
+    let hostname = window.location.hostname;
+
+    if (hostname.includes('zippyshare')) {
+        zippyshare();
+    } else if (hostname.includes('anonfiles')) {
+        anonfiles();
+    } else if (hostname.includes('solidfile')) {
+        solidfile();
+    }
+})();
